6fd44cf2fcb4e8fda5b96686a7e38b8c4100bf2c
A <arianalamar@gmail.com>
2019-11-04 18:53:47 +0100

    added user functions to datarep

--- Task1/API/Program.cs
+++ Task1/API/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace API
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test");
+
         }
     }
 }
--- Task1/Data/DataRepository.cs
+++ Task1/Data/DataRepository.cs
@@ -8,6 +8,42 @@ namespace Data
 {
     public class DataRepository
     {
+        private DataContext context;
 
+        public DataRepository(DataContext c)
+        {
+            this.context = c;
+        }
+
+        #region User
+        public void AddUser(User u)
+        {
+            if (context.users.Exists(x => x.UserID == u.UserID))
+            {
+                throw new Exception("Such an user already exists");
+            }
+            context.users.Add(u);
+        }
+
+        public User GetUser(int id)
+        {
+            if (context.users.Exists(x=>x.UserID == id))
+            {
+                return context.users[id];
+            }
+            throw new Exception("No such an user");
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return context.users;
+        }
+
+        public void DeleteUser(int id)
+        {
+            User tmp = GetUser(id);
+            context.users.Remove(tmp);
+        }
+        #endregion
     }
 }

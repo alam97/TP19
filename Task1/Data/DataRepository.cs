296efb85738c70a9203d7d90fe30fc08fa0942da
A <arianalamar@gmail.com>
2019-11-04 19:10:34 +0100

    added event and inventory functions to datarep

--- Task1/Data/DataRepository.cs
+++ Task1/Data/DataRepository.cs
@@ -15,6 +15,7 @@ namespace Data
             this.context = c;
         }
 
+
         #region User
         public void AddUser(User u)
         {
@@ -44,6 +45,60 @@ namespace Data
             User tmp = GetUser(id);
             context.users.Remove(tmp);
         }
+        #endregion
+
+        #region Event
+        public void AddEvent(Event e)
+        {
+            context.events.Add(e);
+        }
+
+        public Event GetEvent(int id)
+        {
+            return context.events[id];
+        }
+
+        public IEnumerable<Event> GetAllEvents()
+        {
+            return context.events;
+        }
+
+        public void DeleteEvent(int id)
+        {
+           
+        }
+        #endregion
+
+        #region Inventory
+        
+        public void AddInventory(Inventory i)
+        {
+            if (context.inventory.Exists(x => x.InventoryID == i.InventoryID))
+            {
+                throw new Exception("Such an inventory already exists");
+            }
+            context.inventory.Add(i);
+        }
+        public Inventory GetInventory(int id)
+        {
+            if (context.inventory.Exists(x => x.InventoryID == id))
+            {
+                return context.inventory[id];
+            }
+            throw new Exception("No such an inventory");
+        }
+
+        public IEnumerable<Inventory> GetAllInventories()
+        {
+            return context.inventory;
+        }
+
+        public void DeleteInventory(int id)
+        {
+            Inventory tmp = GetInventory(id);
+            context.inventory.Remove(tmp);
+        }
+
         #endregion
     }
 }

d742ea48634649253c5b08929fc7ce1d40322a45
217843 Brylski Aleksander <217843@edu.p.lodz.pl>
2019-11-07 15:17:22 +0100

    public declarations in API

--- Task1/API/Program.cs
+++ Task1/API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Data;
+using Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,14 @@ namespace API
         static void Main(string[] args)
         {
 
-            
+            FillConst fillConst = new FillConst();
+            Shop shop = new Shop(fillConst);
+            User U = new User("Aleksander", "Brylski", 66);
+            shop.CreateUser("Aleksander", "Brylski", 66);
+            Product P = new Product("Knife", 14.21, 7);
+            shop.CreateProduct("Knife", 14.21, 7);
+            shop.AddToInventory(P, 25);
+            shop.BuyItem(shop.Data.GetUser(66), shop.Data.GetProduct(7), 5);
         }
     }
 }

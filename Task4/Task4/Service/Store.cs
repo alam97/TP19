d7b8dfbb933bd1b3137faa4c0e32b5c3cee53ed1
217843 Brylski Aleksander <217843@edu.p.lodz.pl>
2019-12-09 17:59:24 +0100

    store updated

--- Task4/Task4/Service/Store.cs
+++ Task4/Task4/Service/Store.cs
@@ -1,25 +1,21 @@
-﻿using Data;
+﻿using System.Data;
 using System;
-using System.Collections.Generic;
-using System.Text;
-// using System.Data.Linq;
+using System.Linq;
+using Data;
 
-
-namespace Service
+namespace Services
 {
     class Store
 
-
     {
         private LinqToSqlDataContext db;
-
 
         public Store()
         {
             this.db = new LinqToSqlDataContext();
         }
         #region add
-        public void AddUser(Person p)
+        public void AddPerson(Person p)
         {
             db.Persons.InsertOnSubmit(p);
             db.SubmitChanges();
@@ -36,85 +32,155 @@ namespace Service
             db.Events.InsertOnSubmit(e);
             db.SubmitChanges();
 
-
         }
-        public void AddToInventory(Product p, int amount)
+        public void AddToInventory(Inventory inventory)
         {
-
+            db.Inventories.InsertOnSubmit(inventory);
+            db.SubmitChanges();
         }
         #endregion
 
         #region create
         public void CreateUser(string firstName, string lastName, int userId)
         {
-
+            Person person = new Person()
+            {
+                Id = userId,
+                FirstName = firstName,
+                LastName = lastName
+            };
+            AddPerson(person);
         }
         public void CreateProduct(string productName, double productPrice, int productId)
-        { }
+        {
+            Product product = new Product()
+            {
+                Id = productId,
+                Name = productName,
+                Price = (decimal)productPrice
+            };
+            AddProduct(product);
+        }
 
-        public void CreateEvent(Person user, Product catalog, DateTimeOffset date) { }
+        public void CreateEvent(Person user, Product catalog, DateTimeOffset date)
+        {
+            Event ewent = new Event()
+            {
+                PersonId = user.Id,
+                ProductId = catalog.Id,
+                EventDate = date
+            };
+            AddEvent(ewent);
+        }
         #endregion
 
         #region delete
         public void DeleteUser(Person u)
         {
-            //var deleteperson =
-            //    from person in db.Persons
-            //    where person.id == u.id
-            //    select person;
+            var deletePerson =
+                from person in db.Persons
+                where person.Id == u.Id
+                select person;
 
-            //if (deleteperson.count() > 0)
-            //{
-            //    db.persons.deleteonsubmit(deleteperson.first());
-            //    db.submitchanges();
+            if (deletePerson.Count() > 0)
+            {
+                db.Persons.DeleteOnSubmit(deletePerson.First());
+                db.SubmitChanges();
 
-            //}
+            }
         }
         public void DeleteProduct(Product p)
         {
-            //var deleteProduct =
-            //    from product in db.Products
-            //    where product.Id == p.Id
-            //    select person;
+            var deleteProduct =
+                from product in db.Products
+                where product.Id == p.Id
+                select product;
 
-            //if (deleteProduct.Count() > 0)
-            //{
-            //    db.Products.DeleteOnSubmit(deleteProduct.First());
-            //    db.SubmitChanges();
+            if (deleteProduct.Count() > 0)
+            {
+                db.Products.DeleteOnSubmit(deleteProduct.First());
+                db.SubmitChanges();
 
-            //}
+            }
         }
         public void DeleteEvent(Event e)
         {
-            //var deleteEvent =
-            //    from event in db.Events
-            //    where event.Id == e.Id
-            //    select event;
 
-            //if (deleteEvent.Count() > 0)
-            //{
-            //    db.Events.DeleteOnSubmit(deleteEvent.First());
-            //    db.SubmitChanges();
+            var deleteEvent =
+               from ewent in db.Events
+               where ewent.Id == e.Id
+               select ewent;
 
-            //}
+            if (deleteEvent.Count() > 0)
+            {
+                db.Events.DeleteOnSubmit(deleteEvent.First());
+                db.SubmitChanges();
+            }
         }
+
         #endregion
 
         #region update
-        public void UpdateInventory(Product p, int amount) { }
+        public void UpdateInventory(Product p, int amount)
+        {
+            var query =
+                from inventory in db.Inventories
+                where inventory.ProductId == p.Id
+                select inventory;
+
+            foreach (Inventory inv in query)
+            {
+                inv.amount = inv.amount - amount;
+            }
+            db.SubmitChanges();
+        }
         #endregion
 
         #region exists
-        public Boolean ProductExists(Product p) { return true; }
-        public Boolean UserExists(Person u) { return true; }
-        public Boolean ProductExistsinInventory(Product p) { return true; }
+        public Boolean ProductExists(Product p)
+        {
+            var query =
+                from product in db.Products
+                where product.Id == p.Id
+                select product;
+
+            if (query.Count() > 0)
+                return true;
+            else
+                return false;
+        }
+        public Boolean UserExists(Person p)
+        {
+            var query =
+                from person in db.Persons
+                where person.Id == p.Id
+                select person;
+
+            if (query.Count() > 0)
+                return true;
+            else
+                return false;
+        }
+        public Boolean ProductExistsinInventory(Product p)
+        {
+            var query =
+               from inventory in db.Inventories
+               where inventory.ProductId == p.Id
+               select inventory;
+
+            if (query.Count() > 0)
+                return true;
+            else
+                return false;
+        }
         #endregion
 
         #region shop actions
         // buy an item -> creates an invoice, updates inventory
         public void BuyItem(Person user, Product product, int amount)
         {
-
+            CreateEvent(user, product, DateTime.Today);
+            UpdateInventory(product, amount);
         }
 
         #endregion
